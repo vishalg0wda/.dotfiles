382f023f300ff612e0d4c41fd726e51eed8b37b9
Vishal Gowda <cartmanboy1991@gmail.com>
2024-07-12 09:02:24 +0100

    Use rustic mode

--- emacs/init.el
+++ emacs/init.el
@@ -341,7 +341,10 @@
 
 (use-package yasnippet
   :after lsp-mode
+  :config
+  (yas-reload-all)
   :hook ((lsp-mode . yas-minor-mode)))
+
 (use-package yasnippet-snippets)
 
 (use-package flycheck
@@ -360,10 +363,21 @@
   :commands (lsp lsp-deferred)
   :custom
   (lsp-keymap-prefix "C-c l")
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  ;; enable / disable the hints as you prefer:
   (lsp-inlay-hint-enable t)
-  (lsp-rust-analyzer-server-display-inlay-hints t)
-  (lsp-eldoc-render-all nil)
-  :hook rust-mode python-mode java-mode go-mode
+  ;; These are optional configurations. See https://emacs-lsp.github.io/lsp-mode/page/lsp-rust-analyzer/#lsp-rust-analyzer-display-chaining-hints for a full list
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
+  ;; (lsp-eldoc-render-all nil)
+  :hook python-mode java-mode go-mode
   :bind (:map lsp-mode-map
 	      ("C-c l p d" . lsp-ui-peek-find-definitions)
 	      ("C-c l p r" . lsp-ui-peek-find-references)
@@ -379,7 +393,9 @@
   (lsp-ui-sideline-enable nil)
   (lsp-ui-peek-enable t)
   (lsp-ui-imenu-enable t)
-  (lsp-ui-doc-enable t)
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil)
   (lsp-ui-doc-position 'bottom))
 
 (use-package lsp-treemacs
@@ -395,21 +411,38 @@
 
 (use-package go-mode)
 
-
 (use-package tree-sitter-langs
   :ensure t
   :after tree-sitter)
 
-(use-package rust-mode
-  ;; :init (setq rust-mode-treesitter-derive t)
-  :bind (:map rust-mode-map
-	      ("C-c C-c" . rust-run)
-	      ("C-c C-t" . rust-test)
-	      ("C-c C-f" . rust-format-buffer))
-  :custom (rust-format-on-save t))
+(use-package rustic
+  :ensure
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status)
+	      ("C-c C-c C-a" . rustic-cargo-add)
+	      ("C-c C-c C-s" . cargo-process-search))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
 
-(use-package cargo
-  :hook (rust-mode . cargo-minor-mode))
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save t)
+  (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
 
-
-
+(defun rk/rustic-mode-hook ()
+  ;; so that run C-c C-c C-r works without having to confirm, but don't try to
+  ;; save rust buffers that are not file visiting. Once
+  ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
+  ;; no longer be necessary.
+  (when buffer-file-name
+    (setq-local buffer-save-without-query t))
+  (add-hook 'before-save-hook 'lsp-format-buffer nil t))
